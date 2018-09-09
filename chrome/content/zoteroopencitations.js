66df37629a6da9c4e382002e041459c05df79bc8
Philipp Zumstein <zuphilip@gmail.com>
2018-09-09 08:52:38 +0200

    Retrieve newest citations as items

--- chrome/content/zoteroopencitations.js
+++ chrome/content/zoteroopencitations.js
@@ -1,11 +1,15 @@
 Zotero.OpenCitations = {};
 
-Zotero.OpenCitations.init =  function() {
+Zotero.OpenCitations.init = function() {
 	alert("TODO code for init...");
 };
 
-Zotero.OpenCitations.checkOC =  function() {
+Zotero.OpenCitations.checkOC = function() {
+	// TODO: make this configurable by the user
+	// TODO: use config variable to save that number
+	var maxNumberOfItemsToRetrieve = 3;
 	var items = ZoteroPane.getSelectedItems();
+	var currentCollection = ZoteroPane.getSelectedCollection();
 	for (let item of items) {
 		if (!item.isRegularItem() || item.isCollection()) continue;
 		let doi = item.getField('DOI');
@@ -27,10 +31,8 @@ Zotero.OpenCitations.checkOC =  function() {
 		let url = 'http://opencitations.net/index/coci/api/v1/citations/' + doi;
 		req.open('GET', url, true);
 		
-		req.onreadystatechange = function() {
+		req.onreadystatechange = async function() { // async (!) because we want to use await in this function
 			if (req.readyState == 4 && req.status == 200) {
-				let response = JSON.parse(req.responseText);
-				
 				let newNote = new Zotero.Item('note');
 				// check whether open citations note already exists
 				let notes = item.getNotes();
@@ -41,14 +43,18 @@ Zotero.OpenCitations.checkOC =  function() {
 						newNote = note;
 					}
 				}
+				
+				let response = JSON.parse(req.responseText);
+				response.sort((a, b) => new Date(b.creation) - new Date(a.creation));
+				
 				let message = "<h2>Result from <a href='" + url + "'>OpenCitations</a> for " + doi + "</h2>";
 				message += "<p>Found <b>" + response.length + "</b> citations: </p>";
-				response.sort((a, b) => new Date(b.creation) - new Date(a.creation));
 				if (response.length > 0) {
 					let year = response[0].creation.substring(0,4);
 					message += "<h3>" + year + "<h3><ul>";
 					for (let row of response) {
 						if (row.creation.substring(0,4) !== year) {
+							// create a new subsection for each new year
 							year = row.creation.substring(0,4);
 							message += "</ul><h3>" + year + "<h3><ul>";
 						}
@@ -56,8 +62,25 @@ Zotero.OpenCitations.checkOC =  function() {
 						message += " (" + row.creation + ")</li>";
 					}
 					message += "</ul>";
+					
+					let citationCollection = new Zotero.Collection();
+					citationCollection.name = "Newest citations of " + doi;
+					citationCollection.parentID = currentCollection.id;
+					// resolve the returned promise here with await
+					var citationCollectionId = await citationCollection.saveTx();
+					
+					for (let row of response.slice(0, maxNumberOfItemsToRetrieve)) {
+						var translate = new Zotero.Translate.Search();
+						translate.setIdentifier({
+							"DOI": row.citing
+						});
+						translate.setTranslator("11645bd1-0420-45c1-badb-53fb41eeb753"); // CrossRef.js
+						let newItems = translate.translate({
+							libraryID: item.libraryID,
+							collections: [citationCollectionId]
+						});
+					}
 				}
-				//message += JSON.stringify(response, null, '\t');
 				newNote.setNote(message);
 				newNote.parentID = itemID;
 				noteID = newNote.saveTx();
@@ -72,10 +95,10 @@ Zotero.OpenCitations.checkOC =  function() {
 	}
 };
 
-Zotero.OpenCitations.checkWikicite =  function() {
+Zotero.OpenCitations.checkWikicite = function() {
 	alert("TODO code for checking Wikicite...");
 };
 
-Zotero.OpenCitations.checkEPMC =  function() {
+Zotero.OpenCitations.checkEPMC = function() {
 	alert("TODO code for checking EPMC...");
 };
